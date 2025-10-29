0892deb47c1152023db454732b6a7843d9c24c57
JoniMeichtry11 <joni.meichtry@gmail.com>
2025-10-29 15:16:45 -0300

    feat: Add initial Firebase Hosting setup with HTML template and assets
    - Created index.html for Firebase Hosting with necessary Firebase SDK scripts.
    - Added various image assets including social media icons and Open Graph images.
    - Introduced site.webmanifest for PWA support.
    - Updated app.config.ts to initialize Firebase app with configuration.
    - Enhanced index.html with SEO metadata, Open Graph, Twitter Cards, and structured data.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { routes } from './app.routes';
 
@@ -7,6 +12,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'catalogo-testa-hermanos',
+        appId: '1:582569828614:web:80409334ea53801c43be87',
+        storageBucket: 'catalogo-testa-hermanos.firebasestorage.app',
+        apiKey: 'AIzaSyASJdiFkGlRA96kJIRdyIUzWZT_ZzWgu4c',
+        authDomain: 'catalogo-testa-hermanos.firebaseapp.com',
+        messagingSenderId: '582569828614',
+        measurementId: 'G-C1NZKEX56V',
+      })
+    ),
+  ],
 };
